a9fd3ff317df9883fa7947bb68ba2281538f2bbf
José Sánchez-Gallego <gallegoj@uw.edu>
2019-12-18 17:16:14 -0800

    Fix __version__ setting

--- flicamera/__init__.py
+++ flicamera/__init__.py
@@ -12,12 +12,12 @@ import pkg_resources
 
 
 try:
-    __version__ = pkg_resources.get_distribution('flicamera').version
+    __version__ = pkg_resources.get_distribution('sdss-flicamera').version
 except pkg_resources.DistributionNotFound:
     try:
         import toml
         poetry_config = toml.load(open(os.path.join(os.path.dirname(__file__),
-                                                    '../../pyproject.toml')))
+                                                    '../pyproject.toml')))
         __version__ = poetry_config['tool']['poetry']['version']
     except Exception:
         warnings.warn('cannot find flicamera version. Using 0.0.0.', UserWarning)
