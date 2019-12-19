6a95a29cff0ed1738871a8a225227f9003e34ff1
José Sánchez-Gallego <gallegoj@uw.edu>
2019-12-18 17:18:43 -0800

    Add placeholder test

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -3,5 +3,9 @@
 #
 # @Author: José Sánchez-Gallego (gallegoj@uw.edu)
 # @Date: 2019-12-18
-# @Filename: conftest.py
+# @Filename: test_lib.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+
+def test_placeholder():
+    assert True
