56b0e88146f805e3591e1cffabcfa1470eeb2269
José Sánchez-Gallego <gallegoj@uw.edu>
2019-12-20 18:54:02 -0800

    Add initial mocking of devices in tests

--- tests/helpers.py
+++ tests/helpers.py
@@ -10,25 +10,63 @@ import ctypes
 import unittest.mock
 
 
+DEV_COUNTER = 0
+
+
+class MockFLIDevice(object):
+
+    _defaults = {'temperature': {'CCD': 0, 'base': 0},
+                 'serial': 'ML0000',
+                 'exposure_time_left': 0,
+                 'model': 'MicroLine ML50100'}
+
+    def __init__(self, name, **kwargs):
+
+        global DEV_COUNTER
+
+        self.dev = DEV_COUNTER
+        DEV_COUNTER += 1
+
+        self.name = name
+
+        self.reset_defaults()
+        self.state.update(kwargs)
+
+    def reset_defaults(self):
+        """Resets the device to the default state."""
+
+        self.state = self._defaults.copy()
+
+
 class MockLibFLI(ctypes.CDLL):
     """Mocks the CDLL object with the FLI dynamic library."""
 
     def __init__(self, dlpath):
 
         self.dlpath = dlpath
-        self.reset()
+        self.devices = []
 
     def reset(self):
-        """Sets the initial values of the mocked device."""
+        """Resets the initial values of the mocked device."""
 
-        self.temperature = {'CCD': 25, 'base': 20}
+        self.__init__(self.dlpath)
 
     def __getattr__(self, name):
 
-        if name in dir(self):
-            return super(MockLibFLI, self).__getattr__(name)
+        # So for the cases when we haven't overridden the library function,
+        # we returns a Mock that returns 0 (no error).
 
-        return unittest.mock.MagicMock()
+        return unittest.mock.MagicMock(return_value=0)
 
-    def test_method(self):
-        pass
+    def FLIList(self, domain, names_ptr):
+
+        device_names = [(dev.name + ';' + dev.state['model']).encode()
+                        for dev in self.devices]
+
+        # names_ptr is a pointer to a pointer to a char pointer (yep).
+        # We create a char pointer array with the length of the devices and
+        # unpack the above device_names into it.
+        # See https://stackoverflow.com/a/4145859 for details.
+        # Then we access the object to which the names_ptr points to and
+        # replace its contents.
+        names_ptr._obj.contents = (ctypes.c_char_p * len(self.devices))(*device_names)
