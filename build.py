0669148a8363c16413dec994299544d1025be0e5
José Sánchez-Gallego <gallegoj@uw.edu>
2019-12-24 23:29:26 -0800

    Clean up build process

--- build.py
+++ build.py
@@ -15,7 +15,8 @@ import glob
 from setuptools import Extension
 
 
-LIBFLI_PATH = os.path.join(os.path.dirname(__file__), 'cextern/libfli-1.999.1-180223')
+LIBFLI_PATH = os.path.join(os.path.dirname(__file__),
+                           'cextern/libfli-1.999.1-180223')
 
 TRAVIS = os.environ.get('TRAVIS', False)
 
@@ -43,15 +44,15 @@ def get_sources():
     return sources
 
 
-extra_compile_args = ['-D__LIBUSB__', '-Wall', '-O3', '-fPIC', '-g']
-extra_link_args = ['-lm', '-nostartfiles']
+extra_compile_args = ['-O3', '-fPIC', '-g']
+extra_link_args = ['-nostartfiles']
 
 # Do not use libusb on travis because it makes the build fail.
 # This still creates a usable library and we are mocking the device anyway.
 if TRAVIS:
-    libraries = []
+    libraries = ['m']
 else:
-    libraries = ['usb-1.0']
+    libraries = ['m', 'usb-1.0']
 
 
 ext_modules = [
@@ -60,28 +61,12 @@ ext_modules = [
         sources=get_sources(),
         include_dirs=get_directories(),
         libraries=libraries,
+        define_macros=[('__LIBUSB__', '1')],
         extra_compile_args=extra_compile_args,
         extra_link_args=extra_link_args,
         language='c',
         optional=True),
 ]
-
-
-# In case we wanted to compile the grabimage extension, but tests show that it
-# is not faster than the Python ctypes implementation.
-
-# We need to link grabimage against the libfli shared object but Python adds
-# a suffix. We need to figure out the output name.
-# suffix = sysconfig.get_config_var('EXT_SUFFIX')
-# libfli_lib = 'fli' + '.'.join(suffix.split('.')[:-1])
-
-# cython_ext = Extension(
-#     'flicamera.utils.grabimage',
-#     ['flicamera/src/grabimage.pyx'],
-#     libraries=[libfli_lib],
-#     library_dirs=['./flicamera'],
-#     include_dirs=[numpy.get_include()],
-# )
 
 
 def build(setup_kwargs):
