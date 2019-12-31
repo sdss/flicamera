d7b098a6b91826590e76e9ae3198751ee7240146
José Sánchez-Gallego <gallegoj@uw.edu>
2019-12-30 18:25:32 -0800

    Better version parsing

--- flicamera/__init__.py
+++ flicamera/__init__.py
@@ -3,21 +3,30 @@
 # flake8: noqa
 # isort:skip_file
 
-import os
-import warnings
+import pathlib
 
 import pkg_resources
 
 from .utils import get_config, get_logger
 
 
-try:
-    __version__ = pkg_resources.get_distribution('sdss-flicamera').version
-except pkg_resources.DistributionNotFound:
-    __version__ = 'dev'
+def get_version():
+
+    try:
+        return pkg_resources.get_distribution('sdss-flicamera').version
+    except pkg_resources.DistributionNotFound:
+        try:
+            import toml
+            poetry_config = toml.load(
+                open(pathlib.Path(__file__).parent / '../../pyproject.toml'))
+            return poetry_config['tool']['poetry']['version']
+        except Exception:
+            return '0.0.0'
+
 
 NAME = 'flicamera'
 
+__version__ = get_version()
 
 config = get_config(NAME, allow_user=True)
 
