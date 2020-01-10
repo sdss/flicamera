2751ab1fa9e9af5fbef1bc306bcaa48063b5ebdb
José Sánchez-Gallego <gallegoj@uw.edu>
2020-01-09 20:13:42 -0800

    Implementation including exposures

--- flicamera/camera.py
+++ flicamera/camera.py
@@ -6,28 +6,57 @@
 # @Filename: camera.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
-from basecam import BaseCamera, CameraSystem
+import asyncio
+import time
+
+import basecam.exceptions
+from basecam import BaseCamera, CameraEvent, CameraSystem, ExposureError
 
 import flicamera.lib
 
 
+__all__ = ['FLICameraSystem', 'FLICamera']
+
+
 class FLICameraSystem(CameraSystem):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, **kwargs):
 
         self.lib = flicamera.lib.LibFLI()
+        super().__init__(FLICamera, **kwargs)
 
     def list_available_cameras(self):
 
-        return self.lib.list_cameras()
+        # These are camera devices, not UIDs. They can change as cameras
+        # are replugged or moved to a different computer.
+        devices_id = self.lib.list_cameras()
+
+        # Get the serial number as UID.
+        serial_numbers = []
+        for device_id in devices_id:
+            device = flicamera.lib.FLIDevice(device_id, self.lib.libc)
+            serial_numbers.append(device.serial)
+
+        return serial_numbers
 
 
 class FLICamera(BaseCamera):
 
-    async def _connect_internal(self, **config_params):
+    _device = None
+
+    async def _connect_internal(self, serial=None):
         """Internal method to connect the camera."""
 
-        pass
+        if serial is None:
+            serial = self.camera_config.get('uid', None)
+            if not serial:
+                raise basecam.exceptions.CameraConnectionError('unknown serial number.')
+
+        self._device = self.camera_system.lib.get_camera(serial)
+
+        if self._device is None:
+            raise basecam.exceptions.CameraConnectionError(
+                f'cannot find camera with serial {serial}.')
 
     async def _status_internal(self):
         """Gets a dictionary with the status of the camera.
@@ -40,20 +69,62 @@ class FLICamera(BaseCamera):
 
         """
 
-        pass
+        device = self._device
+        device._update_temperature()
 
-    async def _expose_internal(self, exposure_time):
+        return dict(model=device.model,
+                    serial=device.serial,
+                    fwrev=device.fwrev,
+                    hwrev=device.hwrev,
+                    hbib=device.hbin,
+                    vbin=device.vbin,
+                    visible_area=device.get_visible_area(),
+                    image_area=device.area,
+                    temperature_ccd=device._temperature['CCD'],
+                    temperature_base=device._temperature['base'],
+                    exposure_time_left=device.get_exposure_time_left(),
+                    cooler_power=device.get_cooler_power())
+
+    async def _expose_internal(self, exposure_time, image_type='science', **kwargs):
         """Internal method to handle camera exposures.
 
         Returns
         -------
-        fits : `~astropy.io.fits.HDUList`
+        fits : `~astropy.io.fits.HDUList` or `~numpy.array`
             An HDU list with a single extension containing the image data
-            and header.
+            and header, or a 2D Numpy array.
 
         """
 
-        pass
+        TIMEOUT = 5
+
+        device = self._device
+
+        device.cancel_exposure()
+
+        device.set_exposure_time(exposure_time)
+
+        frametype = 'dark' if image_type in ['dark', 'bias'] else 'normal'
+        device.start_exposure(frametype)
+
+        self._notify(CameraEvent.EXPOSURE_INTEGRATING)
+
+        start_time = time.time()
+        time_left = exposure_time
+
+        while True:
+
+            await asyncio.sleep(time_left)
+
+            time_left = device.get_exposure_time_left() / 1000.
+
+            if time_left == 0:
+                self._notify(CameraEvent.EXPOSURE_READING)
+                array = device.read_frame()
+                return array
+
+            if time.time() - start_time > exposure_time + TIMEOUT:
+                raise ExposureError('timeout waiting for exposure to finish.')
 
     @property
     def _uid_internal(self):
@@ -66,9 +137,4 @@ class FLICamera(BaseCamera):
 
         """
 
-        return None
-
-    async def _disconnect_internal(self):
-        """Internal method to disconnect a camera."""
-
-        pass
+        return self.get_status()['serial']
