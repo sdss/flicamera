25c3ad4b4bcb48dc9144a832190eaf9a3581abe6
José Sánchez-Gallego <gallegoj@uw.edu>
2021-02-12 15:48:53 -0800

    Add window group

--- flicamera/model.py
+++ flicamera/model.py
@@ -8,10 +8,54 @@
 
 from __future__ import annotations
 
-from basecam.models import Card, Extension, FITSModel, HeaderModel
+from basecam.models import Card, CardGroup, Extension, FITSModel, HeaderModel
 
 
 __all__ = ["flicamera_model"]
+
+
+window_group = CardGroup(
+    [
+        Card(
+            "BEGX",
+            "{__camera__._device.area[0]}",
+            "Window start pixel in X",
+            default=-999,
+        ),
+        Card(
+            "BEGY",
+            "{__camera__._device.area[1]}",
+            "Window start pixel in Y",
+            default=-999,
+        ),
+        Card(
+            "ENDX",
+            "{__camera__._device.area[2]}",
+            "Window end pixel in X",
+            default=-999,
+        ),
+        Card(
+            "EDNY",
+            "{__camera__._device.area[3]}",
+            "Window end pixel in Y",
+            default=-999,
+        ),
+        Card(
+            "BINX",
+            "{__camera__._device.hbin}",
+            "Binning in X",
+            default=-999,
+        ),
+        Card(
+            "BINY",
+            "{__camera__._device.vbin}",
+            "Binning in Y",
+            default=-999,
+        ),
+    ],
+    name="window",
+    use_group_title=False,
+)
 
 
 apo_raw_header_model = HeaderModel(
@@ -38,6 +82,7 @@ apo_raw_header_model = HeaderModel(
         "STACK",
         "STACKFUN",
         "EXPTIMEN",
+        window_group,
         Card(
             "GAIN",
             "{__camera__.gain}",
@@ -58,6 +103,12 @@ apo_raw_header_model = HeaderModel(
             "Scale of unbinned pixel on sky [arcsec/pix]",
             default=-999.0,
             type=float,
+        ),
+        Card(
+            "OBSERVAT",
+            "{__camera__.observatory}",
+            "Observatory",
+            default="",
         ),
     ]
 )
