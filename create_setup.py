819d95a149da205ec2273fb693f78ccc4e133d11
José Sánchez-Gallego <gallegoj@uw.edu>
2021-05-16 15:28:26 -0700

    Fix raised error

--- create_setup.py
+++ create_setup.py
@@ -37,7 +37,7 @@ try:
     from poetry.__version__ import __version__
 except (ImportError, ModuleNotFoundError) as ee:
     raise ImportError(
-        "install poetry by doing pip install poetry to use this script: {ee}"
+        f"install poetry by doing pip install poetry to use this script: {ee}"
     )
 
 
